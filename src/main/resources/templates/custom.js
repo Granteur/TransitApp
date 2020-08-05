69d84ab3b3cb0be9e534d8617df596a9b46b90ad
AJ Grant <angelojgrant@gmail.com>
2020-08-05 07:57:07 -0400

    updated applicatin.properties to remove api

---
+++ src/main/resources/templates/custom.js
@@ -0,0 +1,19 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
+        zoom: 15,
+        scrollwheel: false
+    });
+
+    for (i = 0; i < busLocations.length; i++) {
+        var image = 'https://maps.google.com/mapfiles/kml/shapes/bus.png'; //adds a Bus icon
+        var marker = new google.maps.Marker({
+            position: {
+                lat: parseFloat(busLocations[i].LATITUDE),
+                lng: parseFloat(busLocations[i].LONGITUDE)
+            },
+            map: map,
+            icon: image
+        });
+}
